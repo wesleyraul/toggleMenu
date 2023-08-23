9db1d8b57f3a91227cd7e441b8a5a8c7e4fe064b
Wesley Raul <wesley.raul@nickbuilder.com.br>
2023-08-23 16:28:18 -0300

    finish

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector(".nav-list");
 const menuIcon = document.querySelector(".mobile-menu");
-console.log('2')
+console.log("2");
 
-menuIcon.addEventListener("click", function() {
-    navToggle.classList.toggle('nav-list-show')
+menuIcon.addEventListener("click", function () {
+  navToggle.classList.toggle("nav-list-show");
 });
